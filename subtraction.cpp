649e04fff151a35cea40a3428fc89b2fbddecbec
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-19 11:33:44 -0700

    subtraction
    subtraction.cpp

---
+++ subtraction.cpp
@@ -0,0 +1,20 @@
+//subtraction
+
+#include <iostream>
+using namespace std;
+main()
+{
+	float num1, num2 ,result;
+	cout<<"enter your first number"<<endl;
+	cin>>num1;
+	cout<<"enter your second  number"<<endl;
+	cin>>num2;
+	result=num1-num2;
+	cout<<"total result is "<<result;
+	
+	return 0;
+	
+	
+	
+	
+}
