5f622f858ff94a088df484874209d47c10117da2
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-19 11:41:35 -0700

    multiplication
    multiplication in cpp

---
+++ multiplicatin.cpp
@@ -0,0 +1,21 @@
+//multiplication
+#include<iostream>
+using namespace std;
+main()
+{
+	float num1,num2,num3;
+	cout<<"enter your first number"<<endl;
+	cin>>num1;
+	cout<<"enter your second number"<<endl;
+	cin>>num2;
+	num3=num1*num2;
+	cout<<"your result is"<<num3;
+		
+		
+		
+		return 0;
+	
+	
+	
+	
+}
