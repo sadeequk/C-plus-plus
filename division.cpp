0b3a01a99850395a7bc6bb1a58bab427ef253158
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-19 11:49:44 -0700

    division.cpp
    division.cpp

---
+++ division.cpp
@@ -0,0 +1,16 @@
+//division
+#include<iostream>
+using namespace std;
+main()
+{   float num1, num2, result;
+	cout<<"enter your first number"<<endl;
+	cin>>num1;
+	cout<<"enter your second number"<<endl;
+	cin>>num2;
+	result=num1/num2;
+	cout<<"your result is "<<result;
+	
+	
+	
+	
+}
