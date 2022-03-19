e4dbcd8fb3c97211d631c606c207645ccd806c31
SADEEQ ULLAH KHAN <101932287+sadeequllahkhan@users.noreply.github.com>
2022-03-19 11:32:36 -0700

    Add files via upload
    addition in c++

---
+++ add.cpp
@@ -0,0 +1,24 @@
+// simple output
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float num1, num2, num3;
+	cout<<"enter your first number "<<endl;
+	cin>>num1;
+		cout<<"enter your second number "<<endl;
+		cin>>num2;
+     num3=num1+num2;
+	cout<<"your result is "<<num3;
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+}
+
