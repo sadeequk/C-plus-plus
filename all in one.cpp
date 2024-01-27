6d64feb0260cac81e1d44789a45f37b6c61cd5b5
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-27 23:17:15 +0500

    Add files via upload

---
+++ all in one.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int p,q,r,s,t;
+	
+	 p=5+2;
+	 q=5-2;
+	 r=5*2;
+     s=5/2;
+	t=5%2;
+	cout<<"addition of 5 and 2 is ="<<p<<endl;
+	cout<<"subtraction of 5 and 2 is ="<<q<<endl;
+	cout<<"multiplication of 5 and 2 is ="<<r<<endl;
+	cout<<"division of 5 and 2 is ="<<s<<endl;
+	cout<<"remainder of 5 and 2 is ="<<t<<endl;
+}
