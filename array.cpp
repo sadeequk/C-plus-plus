eef26d118c67100c4ac568ea0913814025d2ddf8
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-30 23:03:00 +0500

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int data[]={12,33,44,55,66};
+	cout<<"1st element:"<<data[0]<<endl;
+		cout<<"2nd element:"<<data[2]<<endl;
+			cout<<"5th element:"<<data[4]<<endl;
+			return 0;
+}
