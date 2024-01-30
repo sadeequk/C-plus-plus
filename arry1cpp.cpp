5096fc4bd5aa4b6bf16af9519d025e5982fed388
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-30 23:03:58 +0500

    Add files via upload

---
+++ arry1cpp.cpp
@@ -0,0 +1,18 @@
+//array 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a[5];
+	int i;
+	a[0]=9.9;
+	a[1]=12.7;
+	a[2]=5.6;
+	a[3]=6.8;
+	a[4]=8.1;
+	
+	for(i=0; 1<=4; i++)
+	cout<<"value in a["<<i<<"]= "<<a[i]<<endl;
+	
+}
