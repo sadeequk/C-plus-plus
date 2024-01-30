e1f10387132612cdef47a076b5bd01d3e7c8c916
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-01-30 23:06:54 +0500

    Add files via upload

---
+++ cout.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	cout<<"\\ my name is \a khann    \b";
+//	cout<<" i love pakistan "<<endl;
+//	cout<<"A\t B\t C\t D\t \r ";
+
+    cout<<setw(10)<<"pakistan"<<setw(15)<<"swat";
+}
