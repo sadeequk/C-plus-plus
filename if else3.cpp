9be0bec2379152f81685ecbb4826040598aac15f
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-02 23:18:26 +0500

    Add files via upload

---
+++ if else3.cpp
@@ -0,0 +1,19 @@
+//if  else  statement
+#include<iostream>
+using namespace std;
+int main ()
+{
+	 
+	int n ,m;
+	cout<<"Enter your First value"<<endl;
+	cin>>n;
+	cout<<"Enter your Second value"<<endl;
+	cin>>m;
+	if(n%2==m%2)
+	cout<<"The number are equal"<<endl;
+	else
+		cout<<"The number are not equal" <<endl;
+
+
+	
+}
