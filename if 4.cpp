39615ec07e7294c1ca0177bec3ce34d7d47e6f49
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-02 23:17:31 +0500

    Add files via upload

---
+++ if 4.cpp
@@ -0,0 +1,18 @@
+//if statement
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int n,m;
+	cout<<"Enter your First value"<<endl;
+	cin>>n;
+	cout<<"Enter your Second value"<<endl;
+	cin>>m;
+	if(n>m)
+	cout<<"The number"<<n<<"is greater then"<<m<<endl;
+	if(m>n)
+		cout<<"The number"<<m<<"is greater then"<<n<<endl;
+
+
+	
+}
