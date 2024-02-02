2b4d66530491909b57322326dc28605b3eefa286
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-02 23:18:08 +0500

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+main()
+{
+/*	int age;
+	cout<<"enter your age"<<endl;
+	cin>>age;
+	if(age >=18)
+	{
+	cout<<"eligible for vote";
+	}
+	else
+	{                     
+		cout<<"not eligible";
+	}
+	*/
+	
+	
+/*	int password;
+	cout<<"enter your password"<<endl;
+	cin>>password;
+		
+		if(password=0023)
+		{
+			cout<<"wellcome ";
+		}
+		else
+	{
+		cout<<"incorrect password , please enter your corect password and try again";
+	}
+	*/
+	
+	
+	/*
+	      char c1='a';
+	      cout<<"enter your character:";
+	      cin>>c1;
+	      if(isdigit(c1)){     // isdigit defines wether numeric or non-numeric character.
+	      	cout<<"numeric character:";
+		  }
+		  else{
+		  	cout<<"non-numeric charcter :";
+		  }
+		  return 0;   
+		  */
+		  
+		  /*
+		  int x,y;
+		  cout<<"enter the number you want"<<endl;
+		  cin>>x>>y;
+		  
+		  if(x>y)
+		  {
+		  	cout<<"x is greater then y"<<endl;
+		  }
+		  
+		else if(y>x)
+		{
+			cout<<"y is greater then x"<<endl;
+			
+		}
+		else if(y=x)
+		{
+			cout<<"y is equal to x"<<endl;
+			
+		}
+		
+		
+		
+		else{
+			cout<<"not equal";
+		}
+		*/
+		
+		  
+		  int number;
+		  cout<<"enter your number";
+		  cin>>number;
+		  
+		  if(number%2==0)
+		  {
+		  	cout<<"the entered is even"<<endl;
+		  	
+		  }
+		  
+		  else
+		  
+		{
+			cout<<"the entered is odd number";
+		}
+		  
+		  
+		  
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
