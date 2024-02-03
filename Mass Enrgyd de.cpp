8feb66df6bb10aad68dc5b49fb6864cbaccaac09
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-03 22:09:14 +0500

    Add files via upload

---
+++ Mass Enrgyd de.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#define c (3*10^8)
+using namespace std;
+main()
+{
+	
+	int m,energy;
+	cout<<"Enter Mass: "<<endl;
+	cin>>m;
+	energy = m*c^2;
+	cout<<"Energy Body Is: "<<energy;
+	getch();
+	return 0;
+}
