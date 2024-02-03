173d269a1d698caa9ed71a9874278ef76299c418
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-03 22:05:15 +0500

    Add files via upload

---
+++ ifelse 1.cpp
@@ -0,0 +1,17 @@
+//if  else  statement
+#include<iostream>
+using namespace std;
+int main ()
+{
+	 
+	int n;
+	cout<<"Enter your First value"<<endl;
+	cin>>n;
+	if(n%2==1)
+	cout<<"The number"<<n<<"is odd"<<endl;
+	else
+		cout<<"The number"<<n<<"is even"<<endl;
+
+
+	
+}
