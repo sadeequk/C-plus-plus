a065eb5513348d18c69d597027aaff1297946ec2
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-03 22:06:14 +0500

    Add files via upload

---
+++ interchanging value.cpp
@@ -0,0 +1,14 @@
+//interchanging the values by sadeeq ulllah
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+a=200;
+b=100;
+c=a;
+a=b;
+b=c;
+cout<<"enter the value of a="<<a<<endl;
+cout<<"enter the value of b="<<b<<endl;
+}
