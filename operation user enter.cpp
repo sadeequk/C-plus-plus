18429d9aa8797d9788a2d237c78124e61d5df0a3
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-04 22:43:09 +0500

    Add files via upload

---
+++ operation user enter.cpp
@@ -0,0 +1,18 @@
+//age calculater  by sadeeq ulllah
+#include<iostream>
+using namespace std;
+int main(){
+	int n1,n2,s,p,t;
+	cout<<"enter your first number  "<<endl;
+	cin>>n1;
+	cout<<"enter your second number  "<<endl;
+	cin>>n2;
+	s=n1+n2;
+	p=n1*n2;
+	t=n1/n2;
+	cout<<"adition of your entered number is  ="<<s<<endl;
+	cout<<"multiplication of your entered number is  ="<<p<<endl;
+	cout<<"division of your entered number is  ="<<t<<endl;
+}
+
+
