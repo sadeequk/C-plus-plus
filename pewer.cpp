c0680a0812b6bf1fc8ed740db423623f9b310bc0
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-04 22:43:51 +0500

    Add files via upload

---
+++ pewer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+main(){
+	int a,b;
+	cout<<"ENter Your First Value : ";
+	cin>>a;
+	cout<<"ENter Your First Value : ";
+	cin>>b;
+	int result;
+	result =pow(a,b);
+	cout<<"Result is "<<result;
+	return 0;
+}
+
