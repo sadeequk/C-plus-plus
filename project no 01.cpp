27d6e1df7fbf54ddba9ab02c3f51e0f24eea71cc
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-05 22:20:35 +0500

    Add files via upload

---
+++ project no 01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	cout<<"enter the number:";
+	cin>>i;
+	if(i>=0){
+		cout<<"they sould be able to apply:";
+	}
+	else{
+		cout<<"nothing:";
+	}
+}
