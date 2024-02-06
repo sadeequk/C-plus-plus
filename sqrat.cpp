3f6e37a8123788307ce98a084a15b87f71737915
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-06 22:02:12 +0500

    Add files via upload

---
+++ sqrat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int number=36;
+	 double square_root;
+	
+	square_root=sqrt(number);
+	cout<<square_root;
+	
+	return 0;
+	
+}
