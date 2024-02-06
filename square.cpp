43113b764ca388cf7c1e380ea3c318db2dfc9215
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-06 22:02:28 +0500

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+        int number=25;
+        double square_root;
+         square_root=sqrt(number);
+        cout<<square_root<<endl;
+        return 0;
+}
