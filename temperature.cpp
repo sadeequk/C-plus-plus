85c54fee8e20626340923bf26d4b8466885b1220
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-06 22:04:10 +0500

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int celsius;
+	for(celsius=100;celsius>=100;++celsius){
+		cout<<"Celsius:"<<celsius<<"Fahrenheit:"<<((celsius*9/5+32))<<endl;
+		return 0;
+	}
+}
