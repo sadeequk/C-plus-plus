c1d42b255bb68495502fad7fbb6e999442f9c24b
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-07 23:29:59 +0500

    Add files via upload

---
+++ while_loop,c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+	int day;
+	printf("Enter The Name of The day  ");
+	scanf("%d",&day);
+
+	 day=0;
+	while(day<=1){
+              printf("Monday \n");
+              printf("Tuesday \n");
+              printf("Thursday \n");
+              printf("Friday \n");
+              printf("Saturday \n");
+              printf("Sunday \n");
+
+              day++;
+	}
+	printf("\n");
+}
