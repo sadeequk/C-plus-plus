94f82dcd031474672075b4de237d2bc66e82ec79
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-07 23:27:43 +0500

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf(" enter the first value ..\n");
+	scanf("%d",&a);
+	int b;
+	printf(" enter the second value  ...\n");
+	scanf("%d",&b);
+	int c=a-b;
+	printf(" the result is :%d",c);
+}
