e4279b26a31ddeebeebad81e26f793009c51940a
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-17 22:38:18 +0500

    Add files via upload

---
+++ gg.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[3][2]={
+		{2,4},
+		{5,6},
+		{7,8}
+	};
+	
+	for(int i=0;i<3;++i){
+	for(int j=0;j<2;++j){
+		cout<<arr[i][j]<<endl;
+	}
+	int sum=0;
+	cout<<"sum of [i][j] is";
+}
+return 0;
+		
+		
+		
+	
+}
