13ab6bcc10e52e9799a5a7db453079c056b1fa30
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-24 23:23:18 +0500

    Add files via upload

---
+++ total .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int sub1,sub2,sub3,sub4,sub5,sub6,sum;
+	    cout<<"enter your first subject marks:"<<endl;
+	cin>>sub1;
+		cout<<"enter your second subject marks:"<<endl;
+	cin>>sub2;
+		cout<<"enter your third subject marks:"<<endl;
+	cin>>sub3;
+		cout<<"enter your fourth subject marks:"<<endl;
+	cin>>sub4;
+		cout<<"enter your fifth subject marks:"<<endl;
+	cin>>sub5;
+		cout<<"enter your sixth subject marks:"<<endl;
+	cin>>sub6;
+	
+	sum=sub1+sub2+sub3+sub4+sub5+sub6;
+	cout<<"your total is:"<<sum;
+}
