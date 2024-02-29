e3482b795f0948a2e4dabb1bcaa3846b5828bdaf
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-29 23:36:21 +0500

    Add files via upload

---
+++ ADDITIION.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int english,ict,programming,physics,isl,calculs, total;
+	cout<<"Enter your English Marks";
+	cin>>english;
+	cout<<"Enter your ict Marks";
+	cin>>ict;
+		cout<<"Enter your programming Marks";
+	cin>>programming;
+		cout<<"Enter your physics Marks";
+	cin>>physics;
+		cout<<"Enter your isl Marks";
+	cin>>isl;
+		cout<<"Enter your calculs Marks";
+	cin>>calculs;
+	total=english+ict+programming+physics+isl+calculs;
+	cout<<"Sum Of Total Marks="<<total;
+	
+}
